aace2c9b9affa432ca2843f7c6029bd935a7e6a1
Emanuele Zappala <57728217+emazap7@users.noreply.github.com>
2024-07-04 09:45:10 -0600

    Update model.py

--- source/model.py
+++ source/model.py
@@ -10,7 +10,6 @@ if torch.cuda.is_available():
     device = "cuda:0" 
 else:  
     device = "cpu"
-    
 
 
 class F_NN(nn.Module):
@@ -151,3 +150,26 @@ class Leray_Schauder_model(nn.Module):
         out_func = self.recompose(out)
         
         return out_func
+    
+    
+class projected_operator(nn.Module):
+    def __init__(self,in_dim,out_dim,shapes,NL=nn.ELU):
+        super(projected_operator, self).__init__()
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+        self.n_layers = len(shapes) - 1
+        self.shapes = shapes
+        self.first = nn.Linear(in_dim,shapes[0])
+        self.layers = nn.ModuleList([nn.Linear(shapes[i],shapes[i+1]) for i in range(self.n_layers)])
+        self.last = nn.Linear(shapes[-1], out_dim)
+        self.NL = NL(inplace=True) 
+        
+    def forward(self, y):
+        y_in = y
+        y = y_in.flatten(-2,-1)
+        y = self.NL(self.first.forward(y))
+        for layer in self.layers:
+            y = self.NL(layer.forward(y))   
+        y = self.last.forward(y)
+        
+        return y.view(y_in.shape[0],y_in.shape[1],y_in.shape[2]-1,y_in.shape[3])
